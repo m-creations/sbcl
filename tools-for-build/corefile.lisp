09cf8df1de461bd7f03fa298d221ff73a1a74a3a
Andreas Franke <franke@m-creations.com>
2024-08-22 11:51:17 +0200

    [2] Add page_tracks and thread->track .
    tracks.h:
    - Add special tracks: UNUSED_TRACK, INITIAL_TRACK
    - Add accessors: PAGE_TRACK, PAGE_TRACK_SET
    
    * Persist tracks in core image.
    
    * Switch to DEFAULT_TRACK after gen 6 init.
    
      Note that further allocations on initial track(s) will happen later
      when hash-table backing store vectors change.

--- tools-for-build/corefile.lisp
+++ tools-for-build/corefile.lisp
@@ -6,6 +6,7 @@
            #:build-id-core-entry-type-code
            #:directory-core-entry-type-code
            #:initial-fun-core-entry-type-code
+           #:page-tracks-core-entry-type-code
            #:page-table-core-entry-type-code
            #:alien-linkage-table-core-entry-type-code
            #:lisp-linkage-space-core-entry-type-code
@@ -46,6 +47,7 @@
 (defconstant build-id-core-entry-type-code 3860)
 (defconstant directory-core-entry-type-code 3861)
 (defconstant initial-fun-core-entry-type-code 3863)
+(defconstant page-tracks-core-entry-type-code 3879)
 (defconstant page-table-core-entry-type-code 3880)
 (defconstant alien-linkage-table-core-entry-type-code 3881)
 (defconstant lisp-linkage-space-core-entry-type-code 3882)
