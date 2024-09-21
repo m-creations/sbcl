07d210212476df89f5605793c4dff0d25961d2df
Andreas Franke <franke@m-creations.com>
2024-09-21 17:37:30 +0200

    [5] Add test.

--- tests/tracks.impure.lisp
+++ tests/tracks.impure.lisp
@@ -1,1 +1,25 @@
 #-allocation-tracks (invoke-restart 'run-tests::skip-file)
+
+;;;
+;;; an inhomogeneous long list, mixed from different tracks
+;;;
+
+(defconstant +user-tracks+ (- sb-vm:+tracks-end+ 4))
+
+(defun test-long-list-with-mixed-tracks (n)
+  (let ((l nil))
+    (dotimes (i (* n 256))
+      (loop
+        :for tr :from 1 :below +user-tracks+
+        :do (sb-vm:with-track (tr)
+              (push tr l))))
+    (sb-ext:gc :full t)
+    (loop
+      :with tail := l
+      :while tail
+      :do
+         (assert (= (sb-vm:track-of tail) (car tail)))
+         (setf tail (cdr tail)))))
+
+(with-test (:name (:tracks :basic :lists))
+  (test-long-list-with-mixed-tracks 100))
