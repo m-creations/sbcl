5ab92ef763014e3258dafe5180cd1add9ea4d4d7
Andreas Franke <franke@m-creations.com>
2024-10-04 03:51:18 +0200

    [1] Add skeleton for :allocation-tracks feature.
    Add --with-allocation-tracks build option.
    Add tracks.h: include always
    Add tracks.c -- when enabled
    Add tracks.lisp -- when enabled
    Add tracks.impure.lisp for tests.

---
+++ src/runtime/tracks.h
@@ -0,0 +1,19 @@
+/*
+ * separate allocation tracks for dynamic space
+ */
+
+/*
+ * This software is part of the SBCL system. See the README file for
+ * more information.
+ *
+ * This software is derived from the CMU CL system, which was
+ * written at Carnegie Mellon University and released into the
+ * public domain. The software is in the public domain and is
+ * provided with absolutely no warranty. See the COPYING and CREDITS
+ * files for more information.
+ */
+
+#ifndef _TRACKS_H_
+#define _TRACKS_H_
+
+#endif /* _TRACKS_H_ */
