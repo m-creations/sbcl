1d0025527769491e6523ee88592b9776272ef984
Andreas Franke <franke@m-creations.com>
2025-03-08 00:54:22 +0100

    [3] Encode track into page_type when passing it around.

--- src/runtime/gencgc-impl.h
+++ src/runtime/gencgc-impl.h
@@ -321,22 +321,22 @@ void gencgc_apply_code_fixups(struct code *old_code, struct code *new_code);
 extern void gc_close_collector_regions(int);
 
 #define SET_ALLOCATED_BIT(x)
-void *collector_alloc_fallback(struct alloc_region*,sword_t,TRACK_ARG(track_index_t) int);
+void *collector_alloc_fallback(struct alloc_region*,sword_t,int);
 static inline void* __attribute__((unused))
-gc_general_alloc(struct alloc_region* region, sword_t nbytes, TRACK_ARG(track_index_t tr) int page_type)
+gc_general_alloc(struct alloc_region* region, sword_t nbytes, int WITH_TRACK(page_type))
 {
     void *new_obj = region->free_pointer;
     void *new_free_pointer = (char*)new_obj + nbytes;
     // Large objects will never fit in a region, so we automatically dtrt
     if ((new_free_pointer <= region->end_addr)
 #ifdef LISP_FEATURE_ALLOCATION_TRACKS
-        && (PAGE_TRACK(find_page_index(new_obj)) == tr)
+        && (PAGE_TRACK(find_page_index(new_obj)) == TR(page_type))
 #endif
         ) {
         region->free_pointer = new_free_pointer;
         return new_obj;
     }
-    return collector_alloc_fallback(region, nbytes, TRACK_ARG(tr) page_type);
+    return collector_alloc_fallback(region, nbytes, WITH_TRACK(page_type));
 }
 lispobj copy_potential_large_object(lispobj object, sword_t nwords,
                                    struct alloc_region*, int page_type);
@@ -428,15 +428,15 @@ void really_note_transporting(lispobj old,void*new,sword_t nwords);
 extern uword_t gc_copied_nwords, gc_in_situ_live_nwords;
 
 static inline lispobj
-gc_copy_object_(lispobj object, size_t nwords, void* region, TRACK_ARG(track_index_t tr) int page_type)
+gc_copy_object_(lispobj object, size_t nwords, void* region, int WITH_TRACK(page_type))
 {
     CHECK_COPY_PRECONDITIONS(object, nwords);
 #ifdef LISP_FEATURE_ALLOCATION_TRACKS
-    gc_dcheck(PAGE_TRACK(find_page_index((void *)object)) == tr);
+    gc_dcheck(PAGE_TRACK(find_page_index((void *)object)) == TR(page_type));
 #endif
 
     /* Allocate space. */
-    lispobj *new = gc_general_alloc(region, nwords*N_WORD_BYTES, TRACK_ARG(tr) page_type);
+    lispobj *new = gc_general_alloc(region, nwords*N_WORD_BYTES, WITH_TRACK(page_type));
     NOTE_TRANSPORTING(object, new,  nwords);
 
     /* Copy the object. */
@@ -447,25 +447,21 @@ gc_copy_object_(lispobj object, size_t nwords, void* region, TRACK_ARG(track_ind
 static inline lispobj
 gc_copy_object(lispobj object, size_t nwords, void* region, int page_type)
 {
-#ifdef LISP_FEATURE_ALLOCATION_TRACKS
-    page_index_t page = find_page_index((void *)object);
-    track_index_t tr = PAGE_TRACK(page);
-#endif
-    return gc_copy_object_(object, nwords, region, TRACK_ARG(tr) page_type);
+    return gc_copy_object_(object, nwords, region, WITH_TR_FROM_OBJ(object, page_type));
 }
 
 // Like above but copy potentially fewer words than are allocated.
 // ('old_nwords' can be, but does not have to be, smaller than 'nwords')
 static inline lispobj
 gc_copy_object_resizing(lispobj object, long nwords, void* region,
-                        TRACK_ARG(track_index_t tr) int page_type,
+                        int WITH_TRACK(page_type),
                         int old_nwords)
 {
     CHECK_COPY_PRECONDITIONS(object, nwords);
 #ifdef LISP_FEATURE_ALLOCATION_TRACKS
-    gc_dcheck(PAGE_TRACK(find_page_index((void *)object)) == tr);
+    gc_dcheck(PAGE_TRACK(find_page_index((void *)object)) == TR(page_type));
 #endif
-    lispobj *new = gc_general_alloc(region, nwords*N_WORD_BYTES, TRACK_ARG(tr) page_type);
+    lispobj *new = gc_general_alloc(region, nwords*N_WORD_BYTES, WITH_TRACK(page_type));
     NOTE_TRANSPORTING(object, new, old_nwords);
     memcpy(new, native_pointer(object), old_nwords*N_WORD_BYTES);
     return make_lispobj(new, lowtag_of(object));
--- tests/gc-testlib.c
+++ tests/gc-testlib.c
@@ -17,8 +17,9 @@ static void make_instances(int page_type, generation_index_t gen, lispobj result
 {
     page_index_t page = 0, last;
     // Passing SINGLE_OBJECT_FLAG avoids combining with a previous page
-    last = gc_find_freeish_pages(&page, GENCGC_PAGE_BYTES, DEFAULT_TRACK,
-                                 SINGLE_OBJECT_FLAG | page_type, gen);
+    last = gc_find_freeish_pages(&page, GENCGC_PAGE_BYTES,
+                                 TR_PT_ARG(DEFAULT_TRACK, SINGLE_OBJECT_FLAG | page_type),
+                                 gen);
     gc_assert(last == page);
     lispobj* where = (void*)page_address(page);
     page_table[page].type = page_type;
