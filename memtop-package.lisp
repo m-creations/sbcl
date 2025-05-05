6ee88c4921e4eeefb97e52b84ad4071355a4a0fa
Andreas Franke <franke@m-creations.com>
2025-05-05 03:32:11 +0200

    Add initial memtop demo.

---
+++ memtop-package.lisp
@@ -0,0 +1,29 @@
+
+(in-package :cl-user)
+
+(defun resolve-file (path)
+  (merge-pathnames path (or *load-pathname* *compile-file-pathname*)))
+
+(load (compile-file (resolve-file "gc-gen-report-wrapper.lisp")))
+(load (compile-file (resolve-file "gc-gen-report-to-stream.lisp")))
+
+(defmacro try-form (form)
+  `(handler-case
+       (progn
+         (format *error-output* "~&; ~S ... " ',form)
+         (finish-output *error-output*)
+         ,form
+         (format *error-output* "DONE~%"))
+     (serious-condition (e)
+       (format *error-output* "FAILED: ~S~%" e))))
+
+(require 'sb-bsd-sockets)
+(use-package :sb-bsd-sockets)
+
+(try-form (require 'uiop))
+
+(unless (find-package :quicklisp)
+  (try-form (load "~/quicklisp/setup.lisp")))
+
+#+quicklisp
+(ql:quickload :trivial-raw-io)
