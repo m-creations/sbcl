4ce3ea7241998a3bd57fd657f8d2b436909c94ab
Andreas Franke <franke@m-creations.com>
2025-05-15 02:17:55 +0200

    [memtop] Refactor demo code.

--- contrib/sb-memtop/memtop-demo.lisp
+++ contrib/sb-memtop/memtop-demo.lisp
@@ -14,61 +14,134 @@ exit 0
 
 (setf sb-vm::*gc-tracks-report-show-thread-names-p* t)
 
-;; some dummy payloads
+;;
 
-(defvar *lists* nil)
-(defvar *arrays* nil)
-(defvar *hash-tables* nil)
+(defvar *gc-logfile-name*
+  "/tmp/gc-logfile.txt")
 
-(defun periodical-cleanup ()
-  (loop
-    (sleep 10)
-    (setf *lists* nil
-          *arrays* nil
-          *hash-tables* nil)))
+(def-memtop-extension #\L ()
+  ;; toggle gc logging to file
+  (setf (sb-ext:gc-logfile)
+    (if (sb-ext:gc-logfile)
+        nil
+        *gc-logfile-name*)))
 
-(defun alloc-lists ()
-  (loop
-    (sleep 0.001)
-    (sb-ext:atomic-push (make-list 254) *lists*)))
+;; some dummy workers
 
-(defun alloc-arrays ()
-  (loop
-    (sleep 0.001)
-    (sb-ext:atomic-push (make-array 510) *arrays*)))
+#| example:
 
-(defun alloc-hash-tables ()
-  (loop
-    (sleep 0.001)
-    (let ((ht (make-hash-table)))
-      (sb-ext:atomic-push ht *hash-tables*)
-      (loop :for i :below (random 100)
-            :do (setf (gethash i ht) (* i i))))))
+|#
+
+(defvar *worker-configs* nil)
+
+(defmacro def-worker (worker-fun (roots-var
+                                  &key
+                                      char
+                                      (throttle 0.001)
+                                      (reset-interval 10.0))
+                      &body body)
+  (macrolet ((sym! (&rest things)
+               `(sb-int:symbolicate ,@things)))
+    (let ((roots-name* (symbol-name roots-var)))
+      (assert (char= #\* (char roots-name* 0)))
+      (assert (char= #\* (char roots-name* (1- (length roots-name*)))))
+      (let* ((roots-name (subseq roots-name* 1 (1- (length roots-name*))))
+             (period-var (sym! "*RESET-" roots-name "-PERIOD*"))
+             (reset-fun  (sym! "RESET-" roots-name "-LOOP"))
+             (throttle-var (sym! "*THROTTLE-" roots-name "*")))
+        (assert (search roots-name (symbol-name worker-fun)))
+        (let ((c (or char (char roots-name 0))))
+          `(progn
+             ;;
+             (defvar ,roots-var nil)
+             (defvar ,throttle-var ,throttle)
+             (defvar ,period-var ,reset-interval)
+             ;;
+             (defun ,reset-fun ()
+               (loop
+                 (sleep ,period-var)
+                 (setf ,roots-var nil)))
+             ;;
+             (defun ,worker-fun ()
+               (loop
+                 (sleep ,throttle-var)
+                 ,@body))
+             ;;
+             (def-memtop-extension ,c ()
+               (make-thread-with-fresh-track (function ,worker-fun)))
+             ;;
+             (let ((config `(:fun ,',worker-fun :char ,,c :throttle ,',throttle-var
+                             :periodical-reset-fun ,',reset-fun :reset-period ,',period-var)))
+               (push config *worker-configs*))))))))
+
+(def-worker alloc-lists (*lists*)
+  (sb-ext:atomic-push (make-list 254) *lists*))
+
+(def-worker alloc-arrays (*arrays*)
+  (sb-ext:atomic-push (make-array 510) *arrays*))
+
+(def-worker alloc-hash-tables (*hash-tables*)
+  (let* ((n 50 #+nil (random 100))
+         (ht (make-hash-table :size (1- (truncate (* n 1.5))))))
+    (sb-ext:atomic-push ht *hash-tables*)
+    (loop :for i :below n
+          :do (setf (gethash i ht) (* i i)))))
+
+;;
+
+(def-memtop-extension #\w (io-stream)
+  ;; launch a specific worker (designated by the next input char) on a fresh track
+  (let ((c #\A #+nil (read-char* io-stream)))
+    (case c
+      ((#\return #\newline) ;; ignore
+       (setf c (read-char* io-stream))))
+    (assert c)
+    (let ((config (find c *worker-configs*
+                        :key (lambda (config) (getf config :char))
+                        :test 'eql)))
+      (assert config)
+      (let ((f (getf config :fun)))
+        (declare (type function f))
+        (make-thread-with-fresh-track f)))))
 
 (def-memtop-extension #\+ ()
-  (let ((f (case (1+ (random 3))
-             ((1) #'alloc-lists)
-             ((2) #'alloc-arrays)
-             ((3) #'alloc-hash-tables))))
-    (make-thread-with-fresh-track f)))
+  ;; launch a random worker on a fresh track
+  (let* ((worker-config (nth (random (length *worker-configs*)) *worker-configs*))
+         (worker-fun (getf worker-config :fun)))
+    (assert (functionp worker-fun))
+    (make-thread-with-fresh-track worker-fun)))
+
+(defun kill-thread (th)
+  (when (sb-thread:thread-alive-p th)
+    (sb-thread:interrupt-thread th #'sb-thread:abort-thread)
+    (loop
+      :do (sleep 0.001)
+      :while (sb-thread:thread-alive-p th))))
+
+(defun kill-thread-on-track (tr)
+  (assert (typep tr `(integer ,*min-track* ,*max-track*)))
+  (let ((th (aref *track-threads* tr)))
+    (kill-thread th)))
+
+(def-memtop-extension #\k (io-stream)
+  ;; kill the worker associated with the given track
+  (let ((tr (parse-integer (read-line io-stream nil nil nil))))
+    (kill-thread-on-track tr)))
 
 (def-memtop-extension #\- ()
+  ;; kill some worker thread
   (loop
-    :for i :from (1- (fill-pointer *track-threads*)) :downto *min-track*
-    :for th := (aref *track-threads* i)
+    :for tr :from (1- (fill-pointer *track-threads*)) :downto *min-track*
+    :for th := (aref *track-threads* tr)
     :until (and th (sb-thread:thread-alive-p th))
     :finally
        (when th
-         (assert (<= *min-track* i *max-track*))
-         (assert (sb-thread:thread-alive-p th))
-         (sb-thread:interrupt-thread th #'sb-thread:abort-thread)
-         (loop
-           :do (sleep 0.001)
-           :while (sb-thread:thread-alive-p th)))))
+         (kill-thread th))))
 
-;;(setf (sb-ext:gc-logfile) "/tmp/gc-logfile.txt")
-(make-thread* #'periodical-cleanup :name "cleanup")
-(make-thread-with-fresh-track #'alloc-lists)
-(make-thread-with-fresh-track #'alloc-arrays)
-(make-thread-with-fresh-track #'alloc-hash-tables)
+(loop
+  :for config :in *worker-configs*
+  :for reset-fun := (getf config :periodical-reset-fun)
+  :do (make-thread* (symbol-function reset-fun) :name (symbol-name reset-fun)))
+
+(format t "Worker configs:~{~%(~{~S ~S~^~% ~})~}~%" *worker-configs*)
 (serve-memtop)
